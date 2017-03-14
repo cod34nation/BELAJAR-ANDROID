48451f9ef75c0f33b8bf1cb545b562aac723c9f4
cod34nation <afrizalwibisono87@gmail.com>
2017-03-14 17:34:02 +0700

    Belajar 2

---
+++ simple_intent/MyApplication/app/src/main/java/com/example/afrizal/sampleintent/Sub1Activity.java
@@ -0,0 +1,19 @@
+package com.example.afrizal.sampleintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Sub1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+
+
+    }
+}
